d9762d91c35a1fdfe25a9f2466f961a4510e14a9
Dinda Tiara Putri <dindatiara974@gmail.com>
2026-04-05 08:24:23 +0700

    lab 3 revisi

--- main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add_report, name='add_report'),
-    path('edit/<int:pk>/', views.edit_report, name='edit_report'),
-    path('delete/<int:pk>/', views.delete_report, name='delete_report'),
-]
